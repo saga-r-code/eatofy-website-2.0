22a5c46a64e53cbb5fd3316c6a7b9d37cb745317
sagar-workbase-code <shahsagar03@gmail.com>
2024-10-03 02:37:48 +0530

    designing

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client"
 import React, { useEffect } from "react";
-import Image from 'next/image'
 import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
@@ -32,7 +31,7 @@ const Page = () => {
     <div className="overflow-x-hidden">
       {/* HOME */}
       <div className="home w-[100vw] h-[100vh] text-[#ead1b3] overflow-hidden relative">
-        <Image src="./bg-1.jpg" alt="bg" class="bg-img" />
+        <img src="./bg-1.jpg" alt="bg" class="bg-img" />
 
         <nav>
           <ul className="flex justify-evenly  gap-3  items-center  text-sm py-5 px-10 font-semibold tracking-widest md:justify-between md:gap-10 md:text-base">
@@ -64,7 +63,7 @@ const Page = () => {
             Book a Demo
           </button>
         </div>
-        <div className="flex justify-center items-center flex-col  my-3 sm:mt-10  xl:hidden " >
+        <div className="md:flex justify-center items-center flex-col  my-3 sm:mt-10 hidden  xl:hidden " >
           <div className="w-[1px] h-20 bg-[#42464d]"></div>
           <div className="w-5 h-5 border-2 rounded-full border-[#42464d]"></div>
         </div>
@@ -106,12 +105,12 @@ const Page = () => {
             </p>
           </div>
           <div className="about-img order-1 relative ">
-            <Image
+            <img
               src="./about-1.avif"
               alt="about"
               className=" w-[100vw] h-[30vh] sm:h-[40vh] md:w-[60vw] lg:h-[50vh]  object-cover object-center rounded-md hover:scale-105"
             />
-            <Image
+            <img
               src="./about-2.jpg"
               alt="about"
               className="hidden bounce-in-text lg:absolute lg:block top-1/3 -left-10 w-[50vw] h-[30vw] xl:w-[20vw] xl:h-[40vh] object-cover object-center rounded-md"
@@ -131,7 +130,7 @@ const Page = () => {
         </div>
         <div className="service-list xl:w-[80%] mx-auto flex flex-col justify-center items-center  gap-10">
 
-          <div className="headline flex justify-center items-center gap-3">
+          <div className="headline -ml-10 flex justify-center items-center gap-3">
             <div className="flex justify-center items-center">
               <div className="h-[1px] w-5 bg-[#42464d]"></div>
               <div className="h-3 w-3 border-[#42464d] border-2 rounded-full"></div>
@@ -142,7 +141,7 @@ const Page = () => {
           </div>
 
           <div className="service-2 flex flex-col justify-center items-center p-5 gap-5 md:flex-row lg:gap-10 ">
-            <Image
+            <img
               src="./service-3.webp"
               alt="service"
               className="w-fit h-fit  hover:scale-105 transition sm:h-[50vh] md:w-[40vw]  object-contain rounded-2xl xl:w-1/3  xl:h-1/3"
@@ -160,7 +159,7 @@ const Page = () => {
           </div>
 
           <div className="service-1 flex flex-col justify-center items-center p-5 gap-5 md:flex-row lg:gap-10 ">
-            <Image
+            <img
               src="./service-1.avif"
               alt="service"
               className="md:order-2 w-fit h-fit hover:scale-105 transition sm:h-[40vh] md:w-[60vw]  object-contain rounded-2xl  xl:w-1/4 xl:h-1/4"
@@ -178,7 +177,7 @@ const Page = () => {
           </div>
 
           <div className="service-3 flex flex-col justify-center items-center p-5 gap-5 md:flex-row lg:gap-10 ">
-            <Image
+            <img
               src="./service-2.png"
               alt="service"
               className="w-fit h-fit border-2 hover:scale-105 transition border-[#ead1b3] p-5 sm:h-[40vh] md:w-[40vw]  object-contain rounded-2xl xl:w-1/3 xl:h-1/3"
@@ -204,13 +203,13 @@ const Page = () => {
       >
         <header className="info  text-[#ead1b3] h-auto   w-[80%] mx-auto">
 
-          <div className="headline flex justify-center items-center gap-3">
+          <div className="headline -ml-10 flex justify-center items-center gap-3">
             <div className="flex justify-center items-center">
               <div className="h-[1px] w-5 bg-[#42464d]"></div>
               <div className="h-3 w-3 border-[#42464d] border-2 rounded-full"></div>
             </div>
             <h2 className="text-xl lg:text-2xl underline underline-offset-4">
-              Featuresa
+              Features
             </h2>
           </div>
 
@@ -260,19 +259,19 @@ const Page = () => {
                 <h1 className="mb-3 font-semibold text-xl text-[#ead1b3] underline underline-offset-4">
                   Kitchen Order Ticket
                 </h1>
-                <Image src="./feature-1.jpg" alt="feature" />
+                <img src="./feature-1.jpg" alt="feature" />
               </div>
               <div>
                 <h1 className="mb-3 font-semibold text-xl text-[#ead1b3] underline underline-offset-4">
                   Point OF Sale (POS)
                 </h1>
-                <Image src="./feature-2.jpg" alt="feature" />
+                <img src="./feature-2.jpg" alt="feature" />
               </div>
               <div>
                 <h1 className="mb-3 font-semibold text-xl text-[#ead1b3] underline underline-offset-4">
                   Report And Analysis
                 </h1>
-                <Image src="./feature-3.jpeg" alt="feature" />
+                <img src="./feature-3.jpeg" alt="feature" />
               </div>
             </Carousel>
           </div>
@@ -283,59 +282,72 @@ const Page = () => {
       {/* Footer */}
       <div
         id="contact"
-        className="info text-[#ead1b3] w-[100vw] relative p-10 overflow-hidden"
+        className="info text-[#ead1b3] w-[100vw] relative overflow-hidden"
       >
         <div className="blur-effect">
           <div className="absolute -bottom-80 hidden xl:block opacity-35 -left-10 w-[700px] rounded-full h-[500px] blur-3xl bg-[#aa314a] "></div>
           <div className="absolute -bottom-80 opacity-35 -right-10 w-[700px] rounded-full h-[500px] blur-3xl bg-[#267d7c] "></div>
         </div>
-        <section className="bg-[#0f1a28] text-[#ead1b3] px-3 lg:w-[80%] mx-auto h-auto py-20  my-32 flex justify-center items-center gap-10 flex-col rounded-xl">
-          <h2 className="text-xl  underline underline-offset-4 text-center lg:text-2xl">Contact Us</h2>
-          <p className="text-3xl md:text-5xl lg:text-6xl text-center xl:text-7xl xl:w-[90%]">
-            Get in touch with our team to clarify your queries
-          </p>
-          <div className="flex justify-center items-center gap-10 flex-wrap ">
-            <input
-              type="email"
-              id="email"
-              placeholder="YOUR EMAIL.."
-              className="placeholder:text-[#ead1b3] bg-slate-800 border-none  pl-5 py-2 pr-16 md:pr-20 rounded-full border-2 border-slate-600"
-            />
-            <div className="bg-red-400 hover:bg-[#ead1b3] text-[#ead1b3] hover:text-red-400 py-2 px-5 md:pr-20 text-center rounded-full">
-              Join Us
+        <div className="p-2 md:p-0">
+          <section className="bg-[#0f1a28] text-[#ead1b3] px-3 lg:w-[80%]  lg:mx-auto h-auto py-20 mb-20 lg:my-32 flex justify-center items-center gap-10 flex-col rounded-xl">
+            <div className="headline -ml-10 flex justify-center items-center gap-3">
+              <div className="flex justify-center items-center">
+                <div className="h-[1px] w-5 bg-[#42464d]"></div>
+                <div className="h-3 w-3 border-[#42464d] border-2 rounded-full"></div>
+              </div>
+              <h2 className="text-xl lg:text-2xl underline underline-offset-4">
+                Contact Us
+              </h2>
             </div>
-          </div>
-        </section>
-        <h2 className="text-3xl md:text-5xl lg:text-6xl ml-5 mb-5 fade-in-text">Eatofy</h2>
-        <footer className="flex justify-center items-center gap-10 text-slate-200 fade-in-text text-center">
-          <div className="flex justify-center flex-wrap gap-10 md:flex-nowrap">
-            <div className="flex flex-col justify-center items-center xl:justify-center xl:items-center gap-4 w-full md:w-1/3 xl:w-1/3">
-              <h1 className="font-bold text-lg">About Us</h1>
-              <p className="w-1/2 md:w-full">
-                We are dedicated to providing the best restaurant management
-                solutions to streamline your operations and enhance your
-                customer experience.
-              </p>
+            <p className="text-3xl md:text-5xl lg:text-6xl text-center xl:text-7xl xl:w-[90%]">
+              Get in touch with our team to clarify your queries
+            </p>
+            <div className="flex justify-center items-center gap-5 md:gap-10 flex-wrap ">
+              <input
+                type="email"
+                id="email"
+                placeholder="YOUR EMAIL.."
+                className="placeholder:text-[#ead1b3] bg-slate-800 border-none  pl-5 py-2 pr-16 md:pr-20 rounded-full border-2 border-slate-600"
+              />
+              <div className="bg-red-400 hover:bg-[#ead1b3] text-[#ead1b3] hover:text-red-400 py-2 px-5 md:pr-20 text-center rounded-full">
+                Join Us
+              </div>
             </div>
-            <div className="flex flex-col items-center gap-5 w-full md:w-1/3 xl:w-1/3">
-              <h1 className="font-bold text-lg">Service</h1>
-              <ul className="flex flex-col justify-center items-center gap-3">
-                <li>Order Management</li>
-                <li>Inventory Management</li>
-                <li>Video Marketing</li>
-              </ul>
+          </section>
+        </div>
+        <div className="px-2 md:px-10">
+          <h2 className="text-3xl md:text-5xl lg:text-6xl ml-5 mb-5 fade-in-text">Eatofy</h2>
+          
+          <footer className="flex justify-center items-center gap-10 text-slate-200 fade-in-text text-center">
+            <div className="flex justify-center flex-wrap gap-10 md:flex-nowrap">
+              <div className="flex flex-col justify-center items-center xl:justify-center xl:items-center gap-4 w-full sm:w-1/2 md:w-1/3 xl:w-1/3">
+                <h1 className="font-bold text-lg">About Us</h1>
+                <p>
+                  We are dedicated to providing the best restaurant management
+                  solutions to streamline your operations and enhance your
+                  customer experience.
+                </p>
+              </div>
+              <div className="flex flex-col items-center gap-5 w-full md:w-1/3 xl:w-1/3">
+                <h1 className="font-bold text-lg">Service</h1>
+                <ul className="flex flex-col justify-center items-center gap-3">
+                  <li>Order Management</li>
+                  <li>Inventory Management</li>
+                  <li>Video Marketing</li>
+                </ul>
+              </div>
+              <div className="flex flex-col  items-center gap-5 w-full md:w-1/3 xl:w-1/3">
+                <h1 className="font-bold text-lg">Conatact Us</h1>
+                <ul className="flex flex-col justify-center items-center gap-3">
+                  <li>Email:support@eatofy.com</li>
+                  <li>Phone: +91 84199-99929</li>
+                  <li>Address: 123 123 Restaurant St, Food City, FC 12345</li>
+                </ul>
+              </div>
             </div>
-            <div className="flex flex-col  items-center gap-5 w-full md:w-1/3 xl:w-1/3">
-              <h1 className="font-bold text-lg">Conatact Us</h1>
-              <ul className="flex flex-col justify-center items-center gap-3">
-                <li>Email:support@eatofy.com</li>
-                <li>Phone: +91 84199-99929</li>
-                <li>Address: 123 123 Restaurant St, Food City, FC 12345</li>
-              </ul>
-            </div>
-          </div>
-        </footer>
-        <div className="flex justify-center items-center mt-20 ">
+          </footer>
+        </div>
+        <div className="flex justify-center items-center my-20 ">
           <p className="text-white  bottom-5 px-5 text-center md:px-0 md:right-1/3">
             © 2024 Eatofy. All rights reserved. Developed by Appniche
             Technologies
